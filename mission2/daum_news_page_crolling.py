6c3b4bab8af2221e41a83597337edeff988c23dd
haiseong <wjdgotjd9908@gmail.com>
2022-08-13 22:13:32 +0900

    naver news crolling

--- mission2/daum_news_page_crolling.py
+++ mission2/daum_news_page_crolling.py
@@ -2,15 +2,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-request = requests.get('https://news.v.daum.net/v/20220810005210818')
+def detail_info(url):
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, 'html.parser')
 
-soup = BeautifulSoup(request.text, 'html.parser')
+    title = soup.select_one('#cSub > div > h3').text
+    date = soup.select_one('#cSub > div > span > span:nth-child(2) > span').text
+    body = soup.select_one('#harmonyContainer > section').get_text().strip()
 
-title = soup.select_one('#cSub > div > h3').text
+url = 'https://news.v.daum.net/v/20220810005210818'
+title, date, body = detail_info(url)
+
 print(title)
-
-date = soup.select_one('#cSub > div > span > span:nth-child(2) > span').text
 print(date)
-
-body = soup.select_one('#harmonyContainer > section').get_text().strip()
 print(body)
